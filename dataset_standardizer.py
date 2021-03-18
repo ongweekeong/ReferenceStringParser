a96fd374416942c48f654f710bd3ad1a6ba1372b
chowyiyin <yiyin.chow@gamil.com>
2021-03-18 17:34:07 +0800

    Add script to standardize tags in dataset. Standardized datasets in ./dataset/standardized_dataset.json

---
+++ dataset_standardizer.py
@@ -0,0 +1,75 @@
+import re
+import json
+from collections import defaultdict
+"""
+This script is used to standardize the tags in the datasets found in the dataset directory
+""" 
+
+dataset_paths = {
+    "cora": "./dataset/cora-ie/tagged_references.txt",
+    "umass_1": "./dataset/umass-citation/training.docs",
+    "umass_2": "./dataset/umass-citation/testing.docs",
+    "umass_3": "./dataset/umass-citation/dev.docs",
+}
+
+# to collate tags available in dataset (tags are manually matched)
+dataset_tags = dict()
+for dataset in dataset_paths:
+    with open(dataset_paths[dataset], encoding='utf-8', errors='ignore') as f:
+        text = f.read()
+        tags = set(re.findall(r'(?<=<)\w+(?=>)', text))
+        dataset_tags[dataset] = tags
+
+# manual mapping is done to match tags in cora dataset to those in the umass-citation dataset
+cora_to_umass_mapping = {
+    "author": "authors",
+    "booktitle": "booktitle",
+    "date": "date",
+    "editor": "editor",
+    "journal": "journal",
+    "location": "address",
+    "institution": "institution",
+    "note": "note",
+    "publisher": "publisher",
+    "pages": "pages",
+    "tech": "tech",
+    "title": "title",
+    "volume": "volume"
+}
+
+tag_mapping = {
+    "cora": {key: key for key in dataset_tags["cora"]},
+    "umass_1": cora_to_umass_mapping,
+    "umass_2": cora_to_umass_mapping,
+    "umass_3": cora_to_umass_mapping
+}
+
+def extractTagValues(standardized_tags, tag_mapping, reference):
+    result = {standardized_tag : "" for standardized_tag in standardized_tags}
+    for standardized_tag in standardized_tags:
+        reference_tag = tag_mapping[standardized_tag]
+        regex = r'(?<=\<(' + reference_tag + r')\>).*(?=\<\/\1\>)'
+        match = re.search(regex, reference)
+        if match:
+            match = re.sub(r'</?[\w-]+>', '', match.group(0)) # remove any intermediate labels
+            result[standardized_tag] = match
+    return result
+
+result = defaultdict(lambda: "")
+
+for dataset in dataset_paths:
+    with open(dataset_paths[dataset], encoding='utf-8', errors='ignore') as f:
+        references = f.readlines()
+        for reference in references:
+            reference_tagged_dict = extractTagValues(dataset_tags["cora"], tag_mapping[dataset], reference)
+            reference = str.rstrip(re.sub(r'</?[\w-]+>', '', reference)) # strip newline character and remove labels
+            result[reference] = reference_tagged_dict 
+
+output_file = "./dataset/standardized_dataset.json"
+with open(output_file, 'w', errors='ignore') as f:
+    json.dump(result, f)
+
+"""
+For reference:
+print(dataset_tags)
+"""
